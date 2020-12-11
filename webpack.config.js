209f24f47d02f3be2752823a91d41ad2d6264f1d
dwwr <dwwarnerreyes@gmail.com>
2020-12-11 14:07:41 -0800

    deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-// const path = require('path');
+const path = require('path');
 
-// module.exports = {
-//   entry: './src/index.js',
-//   output: {
-//     filename: 'main.js',
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-//   module: {
-//     rules: [
-//     ],
-//   },
-// };
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+    ],
+  },
+};
